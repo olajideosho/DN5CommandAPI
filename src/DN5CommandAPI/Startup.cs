65bf014ad9b6f9cb0d8c30c88169af800f889dd4
Olajide Osho <olajideosho1995@gmail.com>
2021-03-08 09:29:08 +0100

    Trigger CI/CD Test #1

--- src/DN5CommandAPI/Startup.cs
+++ src/DN5CommandAPI/Startup.cs
@@ -47,8 +47,9 @@ namespace DN5CommandAPI
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, CommandContext context)
         {
+            context.Database.Migrate();
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
